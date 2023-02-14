fedc858c256cf2f67e75d3bf96aa2002d9475dc7
florian <seiffarth@cs.uni-bonn.de>
2023-02-14 09:56:15 +0100

    updated test

--- test/hops.h
+++ test/hops.h
@@ -34,7 +34,7 @@ void HopsAutomorphismTest(){
 void HopsTimeTest(){
     GraphStruct triangle = SimplePatterns::Triangle();
     GraphStruct five_clique = SimplePatterns::FullyConnected(5);
-    Hops hops = Hops("../../../GraphData/Hops/com-amazon.ungraph.bgfs", "../test/Results/time_test_");
+    Hops hops = Hops("../../../../GraphData/Hops/com-amazon.ungraph.bgfs", "../Results/time_test_");
     hops.Run(0, triangle, {LABEL_TYPE::UNLABELED,0,1, 100000000, 0});
     hops.Run(0, five_clique, {LABEL_TYPE::UNLABELED,0,1, 100000000, 0});
 }
--- test/test.cpp
+++ test/test.cpp
@@ -3,23 +3,57 @@
 #include "graph_functions.h"
 #include "hops.h"
 
-int main() {
-    //GraphData g_patterns = GraphData<GraphStruct>("../../../GraphData/Hops/patterns/size3.bgf");
-    //LoadSpeedTest();
-    //LoadSaveTest();
-    //LoadSaveTest2();
-    //HopsTest();
-    //HopsAutomorphismTest();
-    //LoadCSV();
-    //HopsRealWorldTest();
-    //HopsSimplePatternsTest();
-    //ConversionTest();
-    HopsTimeTest();
-    //HopsPatternTest();
-    //HopsParallelizationTest();
-    //TestLoadGraphsFromPath("../../../cyclic_hops_cpp/data/patterns/size3","", ".txt");
-    //DijkstraTest();
-    //GraphData g_patterns = GraphData("../../../cyclic_hops_cpp/data/patterns/size4/", "", "", ".txt");
-    GraphData graphs = GraphData<GraphStruct>("../../GraphData/Hops/", "", "cit-hepPh", ".bgfs");
+int main(int argc, char *argv[]) {
+
+    for (int i = 0; i < argc; ++i) {
+        if (std::strcmp(argv[i], "--load_speed") == 0 || std::strcmp(argv[i], "--all") == 0) {
+            LoadSpeedTest();
+        }
+        if (std::strcmp(argv[i], "--load_save") == 0 || std::strcmp(argv[i], "--all") == 0) {
+            LoadSaveTest();
+        }
+        if (std::strcmp(argv[i], "--load_save2") == 0 || std::strcmp(argv[i], "--all") == 0) {
+            LoadSaveTest2();
+        }
+        if (std::strcmp(argv[i], "--hops_simple") == 0 || std::strcmp(argv[i], "--all") == 0) {
+            HopsTest();
+        }
+        if (std::strcmp(argv[i], "--hops_automorphism") == 0 || std::strcmp(argv[i], "--all") == 0) {
+            HopsAutomorphismTest();
+        }
+        if (std::strcmp(argv[i], "--load_csv") == 0 || std::strcmp(argv[i], "--all") == 0) {
+            LoadCSV();
+        }
+        if (std::strcmp(argv[i], "--hops_real_world") == 0 || std::strcmp(argv[i], "--all") == 0) {
+            HopsRealWorldTest();
+        }
+        if (std::strcmp(argv[i], "--hops_simple_patterns") == 0 || std::strcmp(argv[i], "--all") == 0) {
+            HopsSimplePatternsTest();
+        }
+        if (std::strcmp(argv[i], "--conversion") == 0 || std::strcmp(argv[i], "--all") == 0) {
+            ConversionTest();
+        }
+        if (std::strcmp(argv[i], "--hops_time") == 0 || std::strcmp(argv[i], "--all") == 0) {
+            HopsTimeTest();
+        }
+        if (std::strcmp(argv[i], "--hops_pattern") == 0 || std::strcmp(argv[i], "--all") == 0) {
+            HopsPatternTest();
+        }
+        if (std::strcmp(argv[i], "--hops_parallelization") == 0 || std::strcmp(argv[i], "--all") == 0) {
+            HopsParallelizationTest();
+        }
+        if (std::strcmp(argv[i], "--load_graphs") == 0 || std::strcmp(argv[i], "--all") == 0) {
+            TestLoadGraphsFromPath("../../../cyclic_hops_cpp/data/patterns/size3","", ".txt");
+        }
+        if (std::strcmp(argv[i], "--dijkstra") == 0 || std::strcmp(argv[i], "--all") == 0) {
+            DijkstraTest();
+        }
+        if (std::strcmp(argv[i], "--load_pattern") == 0 || std::strcmp(argv[i], "--all") == 0) {
+            GraphData g_patterns = GraphData<GraphStruct>("../../../cyclic_hops_cpp/data/patterns/size4/", "", "",
+                                                          ".txt");
+        }
+        if (std::strcmp(argv[i], "--load_graph") == 0 || std::strcmp(argv[i], "--all") == 0) {
+            GraphData graphs = GraphData<GraphStruct>("../../GraphData/Hops/", "", "cit-hepPh", ".bgfs");
+        }
     int x=0;
 }
