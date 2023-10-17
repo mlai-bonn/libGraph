500bcb12ce1750ceb0ca66a7cf7789426e108a80
florian <seiffarth@cs.uni-bonn.de>
2023-10-17 17:12:18 +0200

    bugfixes

--- executables/test/graph_functions.h
+++ executables/test/graph_functions.h
@@ -151,7 +151,26 @@ bool GraphsToLatex(){
 
 bool TestLayeringTree(){
     // generate some random graph
-    GraphStruct graph = SimplePatterns::ErdosRenyi(10, 20, 0, true);
+    // GraphStruct graph = SimplePatterns::ErdosRenyi(10, 20, 0, true);
+    GraphStruct graph = GraphStruct(15, {});
+    graph.add_edge(0,1);
+    graph.add_edge(0,2);
+    graph.add_edge(1,2);
+    graph.add_edge(1,3);
+    graph.add_edge(2,4);
+    graph.add_edge(3,5);
+    graph.add_edge(4,6);
+    graph.add_edge(4,7);
+    graph.add_edge(5,8);
+    graph.add_edge(5,9);
+    graph.add_edge(6,10);
+    graph.add_edge(6,11);
+    graph.add_edge(7,12);
+    graph.add_edge(8,13);
+    graph.add_edge(9,13);
+    graph.add_edge(9,10);
+    graph.add_edge(11,14);
+    graph.add_edge(12,14);
     TreeEmbeddingAlgorithm treeEmbeddingAlgorithm(graph);
     TreeEmbeddingInputParameters inputParameters = {0,0};
     treeEmbeddingAlgorithm.Run(inputParameters);
--- include/Algorithms/GraphMetricApproximation/TreeEmbedding.h
+++ include/Algorithms/GraphMetricApproximation/TreeEmbedding.h
@@ -53,8 +53,11 @@ private:
 inline LayeringPartition TreeEmbeddingAlgorithm::GetLayeringPartition(const TreeEmbeddingInputParameters& inputParameters){
     std::vector<INDEX> distances;
     GraphStruct::BFSDistances(_graph, inputParameters.root_node_id, distances);
+
     std::vector<std::vector<NodeId>> spheres;
+
     std::vector<std::vector<std::vector<NodeId>>> partition;
+    std::vector<int> clusterIndices = std::vector<int>(_graph.nodes(), -1);
     for (INDEX i = 0; i < distances.size(); ++i) {
         // get the distance
         INDEX distance = distances[i];
@@ -63,41 +66,113 @@ inline LayeringPartition TreeEmbeddingAlgorithm::GetLayeringPartition(const Tree
             partition.emplace_back();
         }
         spheres[distance].push_back(i);
-
     }
 
-    std::vector<bool> visited(_graph.nodes(), false);
-    // start a backward search from the most distant nodes
-    std::vector<NodeId> startNodes = spheres[spheres.size() - 1];
-    INDEX clusterIndex = 0;
-    while (!startNodes.empty()) {
-        for (auto & x : partition){
-            x.emplace_back();
-        }
-        auto startNode = startNodes.back();
-        startNodes.pop_back();
-        std::deque<NodeId> nodes = std::deque<NodeId>();
-        nodes.push_back(startNode);
-        while (!nodes.empty()) {
-            NodeId currentNode = nodes.back();
-            nodes.pop_back();
-            INDEX currentDistance = distances[currentNode];
-            if (!visited[currentNode]) {
-                visited[currentNode] = true;
-                partition[currentDistance][clusterIndex].emplace_back(currentNode);
+    // iterate over the spheres
+    for (int level = (int) spheres.size() - 1; level >= 0; --level) {
+        auto & sphere = spheres[level];
+        // get connected components of the sphere
+        std::vector<std::vector<NodeId>> connectedComponents;
+        // bfs on the sphere
+        std::vector<bool> visited(sphere.size(), false);
+        for (auto startNode : sphere) {
+            if (!visited[startNode]) {
+                visited[startNode] = true;
+                // define a queue for the current backward bfs search
+                std::deque<NodeId> deque = std::deque<NodeId>();
+                deque.push_back(startNode);
+                // count components
+                connectedComponents.emplace_back();
+                connectedComponents.back().emplace_back(startNode);
+                // run the backward bfs search
+                while (!deque.empty()) {
+                    NodeId currentNode = deque.back();
+                    deque.pop_back();
+                    INDEX currentDistance = distances[currentNode];
+                    for (NodeId i = 0; i < _graph.degree(currentNode); ++i) {
+                        NodeId neighbor = _graph.neighbor(currentNode, i);
+                        INDEX neighborDistance = distances[neighbor];
+                        if (neighborDistance >= level) {
+                            deque.push_front(neighbor);
+                            if (neighborDistance == level && !visited[neighbor]){
+                                connectedComponents.back().emplace_back(neighbor);
+                            }
+                        }
 
-                for (NodeId i = 0; i < _graph.degree(currentNode); ++i) {
-                    NodeId neighbor = _graph.neighbor(currentNode, i);
-                    if (!visited[neighbor]) {
+                    }
+                }
+            }
+        }
+        // add the connected components to the partition
+        for (auto & connectedComponent : connectedComponents) {
+            partition[level].emplace_back(connectedComponent);
+        }
+        // TODO contract the graph
+    }
+
+
+
+    std::vector<bool> visited(_graph.nodes(), false);
+    // start a backward search from each of the most distant vertices of the forward search until some visited vertex is reached
+    std::vector<NodeId> nodeOrder;
+    for (auto & sphere : spheres) {
+        for (auto & node : sphere) {
+            nodeOrder.push_back(node);
+        }
+    }
+    while (!nodeOrder.empty()) {
+        auto startNode = nodeOrder.back();
+        nodeOrder.pop_back();
+        if (!visited[startNode]) {
+            // stores the levels of the nodes and the cluster index
+            std::vector<std::pair<int, std::vector<NodeId>>> inverseLocalSpheres = std::vector<std::pair<int, std::vector<NodeId>>>(distances[startNode] + 1, std::make_pair(-1, std::vector<NodeId>()));
+
+            // define a queue for the current backward bfs search
+            std::deque<NodeId> nodes = std::deque<NodeId>();
+            nodes.push_back(startNode);
+            // run the backward bfs search
+            while (!nodes.empty()) {
+                NodeId currentNode = nodes.back();
+                nodes.pop_back();
+                INDEX currentDistance = distances[currentNode];
+                if (inverseLocalSpheres[currentDistance].first == -1 && clusterIndices[currentNode] != -1) {
+                    inverseLocalSpheres[currentDistance].first = clusterIndices[currentNode];
+                }
+                if (!visited[currentNode]) {
+                    visited[currentNode] = true;
+                    inverseLocalSpheres[currentDistance].second.emplace_back(currentNode);
+                    for (NodeId i = 0; i < _graph.degree(currentNode); ++i) {
+                        NodeId neighbor = _graph.neighbor(currentNode, i);
                         INDEX neighborDistance = distances[neighbor];
                         if (neighborDistance <= currentDistance) {
                             nodes.push_front(neighbor);
                         }
                     }
+
                 }
             }
+            // transform the inverse local spheres into a partition
+            int counter = 0;
+            for (auto & inverseLocalSphere : inverseLocalSpheres) {
+                if (inverseLocalSphere.first == -1) {
+                    partition[counter].emplace_back(inverseLocalSphere.second);
+                    // set the cluster indices
+                    for (auto & node : inverseLocalSphere.second) {
+                        clusterIndices[node] = (int) partition[counter].size() - 1;
+                    }
+                }
+                else{
+                    partition[counter][inverseLocalSphere.first].insert(partition[counter][inverseLocalSphere.first].end(), inverseLocalSphere.second.begin(), inverseLocalSphere.second.end());
+                    // set the cluster indices
+                    for (auto & node : inverseLocalSphere.second) {
+                        clusterIndices[node] = inverseLocalSphere.first;
+                    }
+                }
+                ++counter;
+
+
+            }
         }
-        ++clusterIndex;
     }
 
 
